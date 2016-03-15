82948871046da3cf826e83dbef3608185b375811
mikhael <mikhael@milikhin.name>
2016-03-15 13:20:18 +0600

    Update 2016-03-15T07:20:14.910Z

--- src/tasks/css-build.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-
-module.exports = function () {
-	return shell.task([
-		'./node_modules/.bin/r.js -o cssIn=bower_components/r5m-cms/css/all.css out=tmp/engine.css',
-		'./node_modules/.bin/r.js -o cssIn=css/all.css out=dist/engine/lp.css'
-	]);
-};
